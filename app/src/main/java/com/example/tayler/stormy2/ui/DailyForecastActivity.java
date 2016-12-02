4d3d1e4eff31441c860b6d99dadb76785c6f838f
Tayler <taylernbray@gmail.com>
2016-12-02 18:08:45 -0500

    Add Custom List View

--- app/src/main/java/com/example/tayler/stormy2/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/tayler/stormy2/ui/DailyForecastActivity.java
@@ -2,19 +2,20 @@ package com.example.tayler.stormy2.ui;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
 import com.example.tayler.stormy2.R;
+import com.example.tayler.stormy2.adapters.DayAdapter;
+import com.example.tayler.stormy2.weather.Day;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
-        setListAdapter(adapter);
+        DayAdapter adapter = new DayAdapter(this, mDays);
     }
 }
